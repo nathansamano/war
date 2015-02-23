bab52f65e2d76f2b15b784faffd531390d379858
nathansamano <nsv9982@saintjoe.edu>
2015-02-22 20:44:03 -0500

    Update War.java

--- War.java
+++ War.java
@@ -1,13 +1,13 @@
 
 /* Starter routines for 
  * War:  The old card game!
- * Nathan Samano
+ * Nathan Samano (Sophmore Year)
  * December 6, 2013
  */
 
 import javax.swing.*;   	// for ImageIcon and JOptionPane
 
-import java.awt.*;			// for Container
+import java.awt.*;		// for Container
 import java.awt.event.*;	// for ActionListener
 
 public class War extends JFrame implements ActionListener {
@@ -19,15 +19,15 @@ public class War extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;  // to make Eclipse happy
 	
 	//// data members ////
-	private LinkedList player1 = new LinkedList();				// player 1's deck
-	private LinkedList player2 = new LinkedList();				// player 2's deck
+	private LinkedList player1 = new LinkedList();			// player 1's deck
+	private LinkedList player2 = new LinkedList();			// player 2's deck
 	private LinkedList player1InPlay = new LinkedList();		// cards player 1 has in play
 	private LinkedList player2InPlay = new LinkedList();		// cards player 2 has in play
 	private Card player1Card = new Card();		// player 1 faced up card
 	private Card player2Card = new Card();		// player 2 faced up card
-	private int iDeckSize = 0;									// initial size of a deck once generated
-	private int player1Count = 0;				// number of Cards player 1 has left before collection
-	private int player2Count = 0;				// number of Cards player 2 has left before collection
+	private int iDeckSize = 0;			// initial size of a deck once generated
+	private int player1Count = 0;			// number of Cards player 1 has left before collection
+	private int player2Count = 0;			// number of Cards player 2 has left before collection
 	
 	//// change player names variables ////
 	private JTextField field1 = new JTextField();  
@@ -38,14 +38,14 @@ public class War extends JFrame implements ActionListener {
 	//// JFrame window-related variables ////
 	private JPanel player1Panel;			// the panel for player 1
 	private JPanel player2Panel;			// the panel for player 2
-	private JPanel centerPanel;				// the center panel
+	private JPanel centerPanel;			// the center panel
 	private JLabel stackOfCardsImage = null;	// the image for the stack of cards for player 1
 	private JLabel stackOfCardsImage2 = null;	// the image for the stack of cards for player 2
-	private JButton goButton = null;			// button to play next round
+	private JButton goButton = null;		// button to play next round
 	private static final int FRAME_WIDTH = 400;	// initial width of JFrame window
 	private static final int FRAME_HEIGHT = 200;	// initial height
- 	private static final int FRAME_X_POS = 50;		// initial position of JFrame window, in x
-	private static final int FRAME_Y_POS = 50;		// in y
+ 	private static final int FRAME_X_POS = 50;	// initial position of JFrame window, in x
+	private static final int FRAME_Y_POS = 50;	// in y
 
 	//// Constructor ////
 	public War() {
@@ -65,14 +65,14 @@ public class War extends JFrame implements ActionListener {
 	    ////////////////////////////////////////////////////////////////////
 		// set up the content pane -- the inner area of the JFrame Window //
 	    ////////////////////////////////////////////////////////////////////
-		Container contentPane = getContentPane(); 		// the content pane for the main window
+		Container contentPane = getContentPane(); 	// the content pane for the main window
 		contentPane.setLayout(new BorderLayout(10,0));  // 10 pixel gap horizontally, 0 pixel gap vertically
 		contentPane.setBackground(Color.BLACK);
 		
 		////////////////////////////////////////////////////
 		// set up the player 1 panel, add to content pane //
 		////////////////////////////////////////////////////
-		player1Panel = new JPanel();					// for player 1's info
+		player1Panel = new JPanel();	// for player 1's info
 		player1Panel.setBorder(BorderFactory.createLoweredBevelBorder());
 		player1Panel.setLayout(new BorderLayout());
 		player1Panel.add(stackOfCardsImage);
@@ -82,7 +82,7 @@ public class War extends JFrame implements ActionListener {
 		////////////////////////////////////////////////////
 		// set up the player 2 panel, add to content pane //
 		////////////////////////////////////////////////////
-		player2Panel = new JPanel();					// for player 2's info
+		player2Panel = new JPanel();	// for player 2's info
 		player2Panel.setBorder(BorderFactory.createLoweredBevelBorder());
 		player2Panel.setLayout(new BorderLayout());
 		player2Panel.add(stackOfCardsImage2);
@@ -92,7 +92,7 @@ public class War extends JFrame implements ActionListener {
 		//////////////////////////////////////////////////
 		// set up the center panel, add to content pane //
 		//////////////////////////////////////////////////
-		centerPanel = new JPanel();						// for the two cards, and Go button
+		centerPanel = new JPanel();	// for the two cards, and Go button
 		centerPanel.setBorder(BorderFactory.createLoweredBevelBorder());
 		centerPanel.setLayout(new FlowLayout());
 		centerPanel.setBackground(Color.GREEN);
@@ -119,21 +119,21 @@ public class War extends JFrame implements ActionListener {
 		// create a LinkedList object
 		LinkedList deck = new LinkedList();
 		deck.generateDeck(13,4);		// generate full 52 card deck
-		//deck.generateDeck(3,4);			// generate 12 card deck
+		//deck.generateDeck(3,4);		// generate 12 card deck
 		//deck.generateDeck(3,2);		// generate 6 card deck
 		System.out.println("deckSize = " + deck.getDeckSize());		// print out the deck size
-		iDeckSize = deck.getDeckSize();								// set the size of the deck to a variable
-		System.out.println(deck + "\n");							// print the generated deck
-		deck = deck.shuffleDeck(deck.getDeckSize());				// shuffle the deck
-		System.out.println(deck);									// print shuffled deck
+		iDeckSize = deck.getDeckSize();					// set the size of the deck to a variable
+		System.out.println(deck + "\n");				// print the generated deck
+		deck = deck.shuffleDeck(deck.getDeckSize());			// shuffle the deck
+		System.out.println(deck);					// print shuffled deck
 		
 		////////////////////////////////////////
 		// set up players, deal out the cards //
 		////////////////////////////////////////
 		System.out.println(deck.getDeckSize());
-		for (int i=0; i<(iDeckSize/2); i++) {				// pass out all the cards
-			player1.enqueue(deck.dequeue());				// give player 1 a card
-			player2.enqueue(deck.dequeue());				// give player 2 a card
+		for (int i=0; i<(iDeckSize/2); i++) {		// pass out all the cards
+			player1.enqueue(deck.dequeue());	// give player 1 a card
+			player2.enqueue(deck.dequeue());	// give player 2 a card
 		}
 		/***** FOR DEBUGGING PURPOSES *****/
 		/* How many cards in player decks */
@@ -153,8 +153,8 @@ public class War extends JFrame implements ActionListener {
 		// play the round //
 		////////////////////
 		
-		int inPlayCount1 = 0;				// number of Cards player 1 has in play
-		int inPlayCount2 = 0;				// number of Cards player 2 has in play
+		int inPlayCount1 = 0;	// number of Cards player 1 has in play
+		int inPlayCount2 = 0;	// number of Cards player 2 has in play
 		
 		//// flip one Card over in play ////
 		if (player1.count() != 0 /*&& player1 != null*/) {	// as long as player deck is not empty they can play another card
@@ -184,19 +184,19 @@ public class War extends JFrame implements ActionListener {
 		/////// update the frame ///////
 		////////////////////////////////
 		/* update the center panel */
-		centerPanel.removeAll();					// remove old images
+		centerPanel.removeAll();			// remove old images
 		centerPanel.add(player1Card.getImage());  	// add new card image
 		centerPanel.add(player2Card.getImage());	// add new card image
-		centerPanel.add(goButton);					// add the go button
+		centerPanel.add(goButton);			// add the go button
 		centerPanel.add(new JLabel("" + (player1InPlay.count() + player2InPlay.count()) + " cards in play"));
-		centerPanel.validate();						// display the panel
+		centerPanel.validate();				// display the panel
 		
 		// set up the player 1 panel, add to content pane
-		player1Panel.removeAll();					// remove old images
+		player1Panel.removeAll();	// remove old images
 		player1Panel.add(new JLabel(player1Name), BorderLayout.NORTH);  // add new ones
 		player1Panel.add(stackOfCardsImage, BorderLayout.CENTER);
 		player1Panel.add(new JLabel("Cards: " + player1.count()), BorderLayout.SOUTH);
-		player1Panel.validate();					// display the panel
+		player1Panel.validate();	// display the panel
 
 		// set up the player 2 panel, add to content pane
 		player2Panel.removeAll();
@@ -228,13 +228,13 @@ public class War extends JFrame implements ActionListener {
 		//// Player 1 Wins Round ////
 		// player 1's card is greater than player 2's card
 		else if (player1Card.getPointValue() > player2Card.getPointValue()) {
-			inPlayCount1 = player1InPlay.count();				// how many cards player 1 has in play
-			for (int i=0; i<inPlayCount1; i++) {				// dequeue all cards player 1 has in play
+			inPlayCount1 = player1InPlay.count();			// how many cards player 1 has in play
+			for (int i=0; i<inPlayCount1; i++) {			// dequeue all cards player 1 has in play
 				player2InPlay.enqueue(player1InPlay.dequeue());	// enqueue into one middle deck (just used
 			} // for											// player 2's deck rather than making another one)
 			
 			/* shuffle cards before giving to round winner */
-			inPlayCount2 = player2InPlay.count();				// recalculate number of cards middle deck has (player 2's in-play deck)
+			inPlayCount2 = player2InPlay.count();	// recalculate number of cards middle deck has (player 2's in-play deck)
 			player2InPlay = player2InPlay.shuffleDeck(inPlayCount2);
 			
 			for (int i=0; i<inPlayCount2; i++) {
@@ -244,13 +244,13 @@ public class War extends JFrame implements ActionListener {
 		
 		//// Player 2 Wins Round ////
 		else {	/* player1Card.getPointValue() < player2Card.getPointValue() */
-			inPlayCount1 = player1InPlay.count();				// how many cards player 1 has in play
-			for (int i=0; i<inPlayCount1; i++) {				// dequeue all cards player 1 has in play
+			inPlayCount1 = player1InPlay.count();			// how many cards player 1 has in play
+			for (int i=0; i<inPlayCount1; i++) {			// dequeue all cards player 1 has in play
 				player2InPlay.enqueue(player1InPlay.dequeue());	// enqueue into one middle deck (just used
 			} // for											// player 2's deck rather than making another one)
 			
 			/* shuffle cards before giving to round winner */
-			inPlayCount2 = player2InPlay.count();				// recalculate number of cards middle deck has (player 2's in-play deck)
+			inPlayCount2 = player2InPlay.count();			// recalculate number of cards middle deck has (player 2's in-play deck)
 			player2InPlay = player2InPlay.shuffleDeck(inPlayCount2);
 			
 			for (int i=0; i<inPlayCount2; i++) {
